e435938abb1cd238ffab0a2abffc2fb4f4901a06
kimyezzang97 <kimyezzang97@gmail.com>
2025-03-02 17:36:54 +0900

    ref: JWT Filter 정리

--- src/main/java/com/vintly/common/jwt/JWTFilter.java
+++ src/main/java/com/vintly/common/jwt/JWTFilter.java
@@ -1,7 +1,8 @@
 package com.vintly.common.jwt;
 
+import com.vintly.common.exception.memebr.AccessExpiredException;
 import com.vintly.entity.Member;
-import com.vintly.member.model.CustomUserDetails;
+import com.vintly.member.service.CustomUserDetails;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -13,57 +14,38 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class JWTFilter extends OncePerRequestFilter {
+
     private final JWTUtil jwtUtil;
 
     public JWTFilter(JWTUtil jwtUtil) {
-
         this.jwtUtil = jwtUtil;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        // 헤더에서 access키에 담긴 토큰을 꺼냄
+        // 헤더에서 access 키에 담긴 토큰을 꺼냄
         String accessToken = request.getHeader("access");
 
         // 토큰이 없다면 다음 필터로 넘김
-        if (accessToken == null) {
-
+        if (accessToken == null){
             filterChain.doFilter(request, response);
-
-            return;
+            return ;
         }
 
-        // 토큰 만료 여부 확인, 만료시 다음 필터로 넘기지 않음
+        // 토큰 만료 여부 확인, 만료시 401
         try {
             jwtUtil.isExpired(accessToken);
         } catch (ExpiredJwtException e) {
-
-            //response body
-            PrintWriter writer = response.getWriter();
-            writer.print("access token expired");
-
-            //response status code
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return;
+           throw new AccessExpiredException();
         }
 
-        // 토큰이 access인지 확인 (발급시 페이로드에 명시)
+        // 토큰이 access 인지 확인 (발급시 페이로드에 명시)
         String category = jwtUtil.getCategory(accessToken);
 
-        if (!category.equals("access")) {
-
-            //response body
-            PrintWriter writer = response.getWriter();
-            writer.print("invalid access token");
-
-            //response status code
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return;
-        }
+        if (!category.equals("access")) throw new AccessExpiredException();
 
         // username, role 값을 획득
         String username = jwtUtil.getUsername(accessToken);
