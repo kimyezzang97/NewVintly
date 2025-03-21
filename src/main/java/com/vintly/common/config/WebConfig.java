d9a7c16bd1b7f612dedb033dfddff9b3e1dd1853
kimyezzang97 <kimyezzang97@gmail.com>
2025-03-21 10:30:59 +0900

    fix: vue admin login 테스트

--- src/main/java/com/vintly/common/config/WebConfig.java
+++ src/main/java/com/vintly/common/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                //.allowedOriginPatterns("*") //  “*“같은 와일드카드를 사용
-                .allowedOrigins("http://localhost:8080", "http://182.225.186.228")
+                .allowedOriginPatterns("*") //  “*“같은 와일드카드를 사용
+                //.allowedOrigins("http://localhost:8080", "http://182.225.186.228")
                 .allowedMethods("GET", "POST", "PUT","DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("*")
