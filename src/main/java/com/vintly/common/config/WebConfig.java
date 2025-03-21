f9e44f2f327b3c55ecf07509b73cb15e29566a22
kimyezzang97 <kimyezzang97@gmail.com>
2025-03-21 09:31:56 +0900

    fix: vue admin login 테스트

--- src/main/java/com/vintly/common/config/WebConfig.java
+++ src/main/java/com/vintly/common/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*") // “*“같은 와일드카드를 사용
-                .allowedMethods("GET", "POST", "PUT","DELETE")
+                .allowedMethods("GET", "POST", "PUT","DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3600);
