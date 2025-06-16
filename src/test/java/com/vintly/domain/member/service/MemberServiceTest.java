929fa8c809d1830b562e919cff590197190075db
kimyezzang97 <kimyezzang97@gmail.com>
2025-06-16 20:01:10 +0900

    test: MemberService 단위 테스트 - createMember

--- src/test/java/com/vintly/domain/member/service/MemberServiceTest.java
+++ src/test/java/com/vintly/domain/member/service/MemberServiceTest.java
@@ -1,21 +1,31 @@
 package com.vintly.domain.member.service;
 
+import com.vintly.domain.member.Use;
+import com.vintly.domain.member.entity.Member;
 import com.vintly.domain.member.repo.MemberRepository;
+import com.vintly.interfaces.member.MemberRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(MockitoExtension.class)
 public class MemberServiceTest {
 
     @Mock
     private MemberRepository memberRepository;
+
+    @Mock
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @InjectMocks
     private MemberService memberService;
@@ -55,6 +65,48 @@ public class MemberServiceTest {
         Mockito.verify(memberRepository).existsByNickname(nickname);
     }
 
+    @Test
+    @DisplayName("전달받은 Request로 Member를 생성한다.")
+    void createMemberShouldSaveMemberAndReturnEmailCode() {
+        // given
+        MemberRequest.JoinMember join = new MemberRequest.JoinMember(
+                "nickname123",
+                "test@example.com",
+                "secure123@"
+        );
 
+        // 중복 확인시 false 리턴하게 세팅
+        Mockito.when(memberRepository.existsByEmail(join.email())).thenReturn(false);
+        Mockito.when(memberRepository.existsByNickname(join.nickname())).thenReturn(false);
+
+        // 비밀번호 인코더가 특정값 리턴하게 세팅
+        Mockito.when(bCryptPasswordEncoder.encode(join.password())).thenReturn("encoded");
+
+        // 저장시 Member가 리턴된다.
+        Member savedMember = new Member(
+                1L,
+                "test@example.com",
+                "encoded",
+                "nickname123",
+                "123123",
+                "ROLE_USER",
+                Use.K,
+                null
+        );
+        Mockito.when(memberRepository.save(ArgumentMatchers.any(Member.class))).thenReturn(savedMember);
+
+        // when
+        String emailCode = memberService.createMember(join);
+
+        // then
+        assertNotNull(emailCode);
+        assertEquals(6, emailCode.length());
+
+        // 검증
+        Mockito.verify(memberRepository).existsByEmail(join.email());
+        Mockito.verify(memberRepository).existsByNickname(join.nickname());
+        Mockito.verify(bCryptPasswordEncoder).encode(join.password());
+        Mockito.verify(memberRepository).save(ArgumentMatchers.any(Member.class));
+    }
 
 }
